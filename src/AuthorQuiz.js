6c035f451932624d440d566952f6674538892c98
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-05-19 14:35:53 +0200

    Redux for Game State

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,46 +1,47 @@
 import React from 'react';
-import { Link }  from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import logo from './logo.svg';
+import { connect } from 'react-redux'
 import './App.css';
 import './bootstrap.min.css'
 
-function Book({title, onClick}){
-  return (<div className="answer" onClick={() => {onClick(title);}}>
+function Book({ title, onClick }) {
+  return (<div className="answer" onClick={() => { onClick(title); }}>
     <h4>{title}</h4>
   </div>
   )
 }
-function Hero(){
+function Hero() {
   return (
     <div className='row'>
-    <div className='jumbotron col-10 offset-1'>
-      <h1>
-        Author Quize
+      <div className='jumbotron col-10 offset-1'>
+        <h1>
+          Author Quize
       </h1>
-      <p>Select a book written by author shown</p>
-    </div>
+        <p>Select a book written by author shown</p>
+      </div>
     </div>
   );
 }
 
-function Turn({author, books, highlight, onAnswerSelected}){
-  function highlightToBackground(highlight){
+function Turn({ author, books, highlight, onAnswerSelected }) {
+  console.log('highlight:', highlight);
+  function highlightToBackground(highlight) {
     const mapping = {
       'none': '',
       'correct': 'green',
       'wrong': 'red'
-    } 
+    }
 
     return mapping[highlight];
   }
 
-  return (<div className='row turn' style={{backgroundColor: highlightToBackground(highlight)}}>
-    <div className='col-4 offset-1'> 
-    <img src={ author.imageUrl} className="authorImage" alt="author" />
+  return (<div className='row turn' style={{ backgroundColor: highlightToBackground(highlight) }}>
+    <div className='col-4 offset-1'>
+      <img src={author.imageUrl} className="authorImage" alt="author" />
     </div>
     <div className="col-6">
-      { books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
+      {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
     </div>
   </div>)
 }
@@ -56,15 +57,15 @@ Turn.propTypes = {
   highlight: PropTypes.string.isRequired,
   onAnswerSelected: PropTypes.func.isRequired
 }
-function Continue({show, onContinue}){
+function Continue({ show, onContinue }) {
   return (<div className='row'>
     {show ? <div className="col-11">
-      <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button> </div>: null
+      <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button> </div> : null
     }
   </div>)
 }
 
-function Footer(){
+function Footer() {
   return (
     <div id="footer" className="row">
       <div className="col-12">
@@ -72,20 +73,41 @@ function Footer(){
           All Images From <a href="wikimedia.com">wikimedia commons</a>
         </p>
       </div>
-      </div>
-  );
-}
-
-function AuthorQuiz({turnData, highlight, onAnswerSelected, onContinue}) {
-  return (
-    <div className="container-fluid">
-      <Hero />
-      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
-      <Continue  show={ highlight == 'correct' } onContinue={onContinue}/>
-      <p><Link to="/add">Add an Author</Link></p>
-      <Footer />
     </div>
   );
 }
+
+function mapStateToProps(state){
+  return {
+    turnData: state.turnData,
+    books: state.books,
+    highlight: state.highlight
+  };
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    onAnswerSelected: (answer) => {
+      dispatch({type: 'ANSWER_SELECTED', answer}) // <-- here answer is dispatched to reducer
+    },
+    onContinue: () => {
+      dispatch({'type': 'CONTINUE'})
+    }
+  }
+}
+
+const AuthorQuiz = connect(mapStateToProps, mapDispatchToProps)(
+  function({ turnData, highlight, onAnswerSelected, onContinue }) {
+    return (
+      <div className="container-fluid">
+        <Hero />
+        <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+        <Continue show={highlight === 'correct'} onContinue={onContinue} />
+        <p><Link to="/add">Add an Author</Link></p>
+        <Footer />
+      </div>
+    );
+  }
+);
 
 export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom'
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
+
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import AddAuthorForm from './AddAuthorForm'
@@ -69,50 +72,53 @@ function getTrunData() {
   }
 }
 
-function resetState(){
-  return  {
-    turnData: getTrunData(),
-    highlight: ''
+
+
+function reducer(state = { authors, turnData: getTrunData(authors), highlight: '' }, action) {
+
+  switch (action.type) {
+    case 'ANSWER_SELECTED':
+      const isCorrect = state.turnData.author.books.some((book) => book === action.answer);
+      return Object.assign({}, state, 
+        { highlight: isCorrect ? 'correct' : 'wrong' }
+        );
+    case 'CONTINUE':
+      return Object.assign({}, state, 
+        {highlight: '', turnData: getTrunData(state.authors)
+      });
+    default:
+      return state;
   }
 }
-let state = {
-  turnData: getTrunData(),
-  highlight: ''
-}
 
-function onAnswerSelected(answer) {
-  const isCorrect = state.turnData.author.books.some((book) => book === answer);
-  state.highlight = isCorrect ? 'correct' : 'wrong';
-  render();
-}
+let store = Redux.createStore(reducer);
 
 function App() {
-  return <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} onContinue={() => {
-    state = resetState();
-    render();
-  }} />
+  return <ReactRedux.Provider store={store}>
+    <AuthorQuiz />
+  </ReactRedux.Provider>
 }
 
-const AuthorWrapper = withRouter(({history}) => 
-   <AddAuthorForm onAddAuthor={(author) => {
+const AuthorWrapper = withRouter(({ history }) =>
+  <AddAuthorForm onAddAuthor={(author) => {
     authors.push(author);
     history.push('/');
   }} />
 )
 
-function render() {
-  ReactDOM.render(
-    <Router>
-      <React.Fragment>
+
+ReactDOM.render(
+  <Router>
+    <React.Fragment>
       <Route exact path="/" component={App} />
       <Route path="/add" component={AuthorWrapper} />
-      </React.Fragment>
-      
-    </Router>
-    , document.getElementById('root'));
-}
+    </React.Fragment>
 
-render();
+  </Router>
+  , document.getElementById('root'));
+
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
